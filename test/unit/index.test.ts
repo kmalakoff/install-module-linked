5e1a94e0e0442806450511db31a1d87eb0b80759
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 15:38:30 -0800

    refactor: patch global Promise in tests and update dependencies

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,3 +1,6 @@
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+import Promise from 'pinkie-promise';
+
 import assert from 'assert';
 import fs from 'fs';
 import path from 'path';
@@ -15,45 +18,53 @@ const CACHE_DIR = path.join(TMP_DIR, 'cache');
 const NODE_MODULES = path.join(TMP_DIR, 'node_modules');
 
 describe('install-module-linked node', () => {
-  beforeEach((cb) => {
-    const queue = new Queue();
-    queue.defer(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
-    queue.defer(mkdirp.bind(null, NODE_MODULES));
-    queue.await(cb);
-  });
-  after(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
-
-  it('install callback (with version)', (done) => {
-    installModule('each-package@0.7.1', NODE_MODULES, { cacheDirectory: CACHE_DIR }, (err) => {
-      assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
-      const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
-      assert.equal(packageJSON.name, 'each-package');
-      assert.equal(packageJSON.version, '0.7.1');
-      done(err);
+  (() => {
+    // patch and restore promise
+    let rootPromise: Promise;
+    before(() => {
+      rootPromise = global.Promise;
+      global.Promise = Promise;
     });
-  });
-
-  it('install callback (no version)', (done) => {
-    installModule('each-package', NODE_MODULES, { cacheDirectory: CACHE_DIR }, (err) => {
-      assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
-      const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
-      assert.equal(packageJSON.name, 'each-package');
-      assert.ok(packageJSON.version.length);
-      done(err);
+    after(() => {
+      global.Promise = rootPromise;
     });
-  });
+  })();
 
-  it('install hasPromise', (done) => {
-    installModule('each-package@0.4.2', NODE_MODULES, { cacheDirectory: CACHE_DIR })
-      .then((_value) => {
+  describe('setup tests', () => {
+    beforeEach((cb) => {
+      const queue = new Queue();
+      queue.defer(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
+      queue.defer(mkdirp.bind(null, NODE_MODULES));
+      queue.await(cb);
+    });
+    after(rimraf2.bind(null, TMP_DIR, { disableGlob: true }));
+
+    it('install callback (with version)', (done) => {
+      installModule('each-package@0.7.1', NODE_MODULES, { cacheDirectory: CACHE_DIR }, (err) => {
         assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
         const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
         assert.equal(packageJSON.name, 'each-package');
-        assert.equal(packageJSON.version, '0.4.2');
-        done();
-      })
-      .catch((err) => {
+        assert.equal(packageJSON.version, '0.7.1');
         done(err);
       });
+    });
+
+    it('install callback (no version)', (done) => {
+      installModule('each-package', NODE_MODULES, { cacheDirectory: CACHE_DIR }, (err) => {
+        assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
+        const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
+        assert.equal(packageJSON.name, 'each-package');
+        assert.ok(packageJSON.version.length);
+        done(err);
+      });
+    });
+
+    it('install (promise)', async () => {
+      await installModule('each-package@0.4.2', NODE_MODULES, { cacheDirectory: CACHE_DIR });
+      assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
+      const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
+      assert.equal(packageJSON.name, 'each-package');
+      assert.equal(packageJSON.version, '0.4.2');
+    });
   });
 });
