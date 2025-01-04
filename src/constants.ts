2d41aa6f2df2dd40c9700944cee473bc9e9806c6
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-04 14:19:34 -0800

    Upgrade modules

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ import path from 'path';
 
 import homedir from 'homedir-polyfill';
 
-export const DEFAULT_CACHE_PATH = path.join(homedir(), '.im', 'cache');
+export const DEFAULT_CACHE_PATH = path.join(homedir(), '.iml');
