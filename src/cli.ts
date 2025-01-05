37e08ba41ec2dc2ce79cc050f25bdd5755326128
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-04 16:43:54 -0800

    refactor: inline clean command and move cache logic to lib

--- src/cli.ts
+++ src/cli.ts
@@ -2,31 +2,28 @@
 
 import exit from 'exit';
 import getopts from 'getopts-compat';
-import clean from './cache/clean';
+import rimraf2 from 'rimraf2';
+import { DEFAULT_CACHE_PATH } from './constants';
 
 export default (argv) => {
   const options = getopts(argv, { stopEarly: true });
 
   const args = options._;
   if (!args.length) {
-    console.log('Missing command. Example usage: im [command]');
+    console.log('Missing command. Example usage: iml [command]');
     return exit(-1);
   }
 
-  switch (args[0]) {
-    case 'clean': {
-      return clean({}, (err) => {
-        if (err) {
-          console.log(err.message);
-          return exit(err.code || -1);
-        }
-        console.log(`Clean ${err ? `failed: ${err.message}` : 'succeeded'}`);
-        return exit(err ? -1 : 0);
-      });
+  if (args[0] === 'clean') {
+    try {
+      rimraf2.sync(DEFAULT_CACHE_PATH, { disableGlob: true });
+      console.log(`Cleaned ${DEFAULT_CACHE_PATH}`);
+    } catch (_) {
+      console.log(`Nothing ti clean at ${DEFAULT_CACHE_PATH}`);
     }
-    default: {
-      console.log(`Unrecognized command: ${argv[0]}. Example usage: im [command]`);
-      return exit(-1);
-    }
+    return exit(0);
   }
+
+  console.log(`Unrecognized command: ${argv[0]}. Example usage: im [command]`);
+  return exit(-1);
 };
