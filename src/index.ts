948938b242f392b8f27800d7e38eb7f99270fc6f
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-08 07:11:37 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import worker from './worker';
+import workerAsync from './workers/async';
+import workerSync from './workers/sync.cjs';
 
 import type { InstallCallback, InstallOptions } from './types';
 
@@ -12,6 +13,10 @@ export default function installModule(installString: string, nodeModulesPath: st
     options = {};
   }
 
-  if (typeof callback === 'function') return worker(installString, nodeModulesPath, options, callback) as undefined;
-  return new Promise((resolve, reject) => worker(installString, nodeModulesPath, options, (err, dest) => (err ? reject(err) : resolve(dest))));
+  if (typeof callback === 'function') return workerAsync(installString, nodeModulesPath, options, callback) as undefined;
+  return new Promise((resolve, reject) => workerAsync(installString, nodeModulesPath, options, (err, dest) => (err ? reject(err) : resolve(dest))));
+}
+
+export function sync(installString: string, nodeModulesPath: string, options: InstallOptions): string | undefined {
+  return workerSync(installString, nodeModulesPath, options);
 }
