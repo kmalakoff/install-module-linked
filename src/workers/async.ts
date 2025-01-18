c954f1b29a466ee607668d4c68a5455fa5ce3d5a
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-17 18:03:33 -0800

    add lock to prevent concurrent module installations

--- src/workers/async.ts
+++ src/workers/async.ts
@@ -7,31 +7,38 @@ import { DEFAULT_CACHE_PATH } from '../constants';
 import cache from '../lib/cache';
 import parseInstallString from '../lib/parseInstallString';
 
+import { Lock } from 'lock';
+const lock = Lock();
+
 const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
 const symlinkType = isWindows ? 'junction' : 'dir';
 
 import type { InstallCallback, InstallOptions } from '../types';
 
 export default function installModule(installString: string, nodeModulesPath: string, options: InstallOptions, callback: InstallCallback): void {
-  const cachePath = options.cachePath || DEFAULT_CACHE_PATH;
-  const { name } = parseInstallString(installString);
-  const dest = path.join(nodeModulesPath, ...name.split('/'));
+  lock([installString, nodeModulesPath], (release) => {
+    callback = release(callback);
 
-  fs.stat(dest, (err) => {
-    if (!err) return callback(null, dest); // already installed
+    const cachePath = options.cachePath || DEFAULT_CACHE_PATH;
+    const { name } = parseInstallString(installString);
+    const dest = path.join(nodeModulesPath, ...name.split('/'));
 
-    cache(installString, cachePath, (err, cachedAt) => {
-      if (err) {
-        console.log(`Could not install: ${installString}. Message: ${err.message}`);
-        return callback();
-      }
-      const queue = new Queue(1);
-      queue.defer(mkdirp.bind(null, path.dirname(dest)));
-      queue.defer(rimraf2.bind(null, dest, { disableGlob: true }));
-      queue.defer(fs.symlink.bind(null, cachedAt, dest, symlinkType));
-      queue.defer(fs.stat.bind(null, dest));
-      queue.await((err) => {
-        err ? callback(err) : callback(null, dest);
+    fs.stat(dest, (err) => {
+      if (!err) return callback(null, dest); // already installed
+
+      cache(installString, cachePath, (err, cachedAt) => {
+        if (err) {
+          console.log(`Could not install: ${installString}. Message: ${err.message}`);
+          return callback();
+        }
+        const queue = new Queue(1);
+        queue.defer(mkdirp.bind(null, path.dirname(dest)));
+        queue.defer(rimraf2.bind(null, dest, { disableGlob: true }));
+        queue.defer(fs.symlink.bind(null, cachedAt, dest, symlinkType));
+        queue.defer(fs.stat.bind(null, dest));
+        queue.await((err) => {
+          err ? callback(err) : callback(null, dest);
+        });
       });
     });
   });
