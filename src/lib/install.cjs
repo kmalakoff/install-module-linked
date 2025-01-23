fa9c9d2438da30109a33b6937018248e8cba1127
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 08:56:55 -0800

    Upgrade modules

--- src/lib/install.cjs
+++ src/lib/install.cjs
@@ -1,12 +1,24 @@
+const path = require('path');
 const spawn = require('cross-spawn-cb');
+const { spawnOptions } = require('node-version-utils');
 
+const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE);
 const major = +process.versions.node.split('.')[0];
 
+let execPath = null;
 module.exports = function install(specifier, dest, callback) {
   if (major > 0) return spawn('npm', ['install', specifier], { cwd: dest }, callback);
 
+  if (!execPath) {
+    const satisfiesSemverSync = require('node-exec-path').satisfiesSemverSync;
+    execPath = satisfiesSemverSync('>0'); // must be more than node 0.12
+    if (!execPath) return callback(new Error('install-module-linked a version of node >0 to use npm install'));
+  }
+
   try {
-    const res = require('node-version-call')({ version: 'stable', callbacks: true }, __filename, specifier, dest);
+    const installPath = isWindows ? path.join(execPath, '..') : path.join(execPath, '..', '..');
+    const options = spawnOptions(installPath, { execPath, callbacks: true });
+    const res = require('function-exec-sync')(options, __filename, specifier, dest);
     callback(null, res);
   } catch (err) {
     callback(err);
