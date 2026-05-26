ca5718b0649296ebbbadf8cb6eda5cf8786d1e47
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:18 -0700

    Fix broken types

--- src/lib/parseInstallString.ts
+++ src/lib/parseInstallString.ts
@@ -1,6 +1,6 @@
 export default function parseInstallString(installString: string) {
   const installParts = installString.split('/');
-  const nameVersionParts = installParts.pop().split('@');
+  const nameVersionParts = (installParts.pop() ?? '').split('@');
   const version = nameVersionParts.length > 1 ? nameVersionParts.pop() : undefined;
   const name = [...installParts, nameVersionParts.join('@')].join('/');
   return {
