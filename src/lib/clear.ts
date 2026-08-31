8e51356ffd12ba3c7135c873f13dd81be51bf882
Kevin Malakoff <kmalakoff@gmail.com>
2026-08-30 21:27:33 -0700

    refactor: enhance clear function to support custom homedir and update tests

--- src/lib/clear.ts
+++ src/lib/clear.ts
@@ -3,7 +3,7 @@ import { homedir, rmSync } from '../compat.ts';
 import type { CleanOptions } from '../types.ts';
 
 export default function clear(options?: CleanOptions) {
-  const cachePath = path.join(homedir(), '.iml');
+  const cachePath = path.join((options?.homedir ?? homedir)(), '.iml');
   (options?.rmSync ?? rmSync)(cachePath);
   console.log(`Cleaned ${cachePath}`);
 }
