e0b56ee7d5cb0bdece3c9751c92ea5c853d0bacb
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:37:56 -0700

    Add lock to prevent concurrent module installations

--- test/unit/promise.test.ts
+++ test/unit/promise.test.ts
@@ -14,6 +14,7 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 const TMP_DIR = path.join(__dirname, '..', '..', '.tmp');
 const CACHE_DIR = path.join(TMP_DIR, 'cache');
 const NODE_MODULES = path.join(TMP_DIR, 'installed', 'node_modules');
+const STRESS_COUNT = 10;
 
 describe('install-module-linked (promise)', () => {
   (() => {
@@ -49,6 +50,24 @@ describe('install-module-linked (promise)', () => {
 
     it('install no version', async () => {
       await installModule('each-package', NODE_MODULES, { cachePath: CACHE_DIR });
+      assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
+      const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
+      assert.equal(packageJSON.name, 'each-package');
+      assert.ok(packageJSON.version.length);
+    });
+
+    it('link multiple (serial)', async () => {
+      for (let counter = 0; counter < STRESS_COUNT; counter++) {
+        await installModule('each-package', NODE_MODULES, { cachePath: CACHE_DIR });
+      }
+      assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
+      const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
+      assert.equal(packageJSON.name, 'each-package');
+      assert.ok(packageJSON.version.length);
+    });
+
+    it('link multiple (parallel)', async () => {
+      await Promise.all([...Array(STRESS_COUNT)].map((_) => installModule('each-package', NODE_MODULES, { cachePath: CACHE_DIR })));
       assert.ok(fs.existsSync(path.join(NODE_MODULES, 'each-package')));
       const packageJSON = JSON.parse(fs.readFileSync(path.join(NODE_MODULES, 'each-package', 'package.json'), 'utf8'));
       assert.equal(packageJSON.name, 'each-package');
