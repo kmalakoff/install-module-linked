a33caf21bab266138cff6ff9b6853bd8a4edbe54
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:45:23 -0700

    Use safeRm and add rename fallback for concurrent installs

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { rm } from 'fs-remove-compat';
+import { safeRm } from 'fs-remove-compat';
 import mkdirp from 'mkdirp-classic';
 import path from 'path';
 import Queue from 'queue-cb';
@@ -8,6 +8,7 @@ import type { EnsureCachedCallback } from '../types.ts';
 import getSpecifier from './getSpecifier.ts';
 import install from './install.cjs';
 import parse from './parseInstallString.ts';
+import renameWithFallback from './renameWithFallback.ts';
 
 export default function ensureCached(installString: string, cachePath: string, callback: EnsureCachedCallback) {
   getSpecifier(installString, (err, specifier) => {
@@ -25,11 +26,11 @@ export default function ensureCached(installString: string, cachePath: string, c
       queue.defer(mkdirp.bind(null, tmp));
       queue.defer(fs.writeFile.bind(null, path.join(tmp, 'package.json'), '{}', 'utf8'));
       queue.defer(install.bind(null, specifier, tmp));
-      queue.defer((cb) => fs.rename(tmpModulePath, cachedAt, cb.bind(null, null)));
-      queue.defer((cb) => fs.rename(path.join(tmp, 'node_modules'), path.join(cachedAt, 'node_modules'), cb.bind(null, null)));
+      queue.defer((cb) => renameWithFallback(tmpModulePath, cachedAt, cb));
+      queue.defer((cb) => renameWithFallback(path.join(tmp, 'node_modules'), path.join(cachedAt, 'node_modules'), cb));
       queue.await((err) => {
         // clear up whether installed or not
-        rm(tmp, () => (err ? callback(err) : callback(null, cachedAt)));
+        safeRm(tmp, () => (err ? callback(err) : callback(null, cachedAt)));
       });
     });
   });
